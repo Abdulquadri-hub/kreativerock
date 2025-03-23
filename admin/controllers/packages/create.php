1d2c220362ffb189e66f259ed2c39ba640e9202e
Abdul Quadri <abdulquadri.aq@gmail.com>
2025-03-23 14:50:12 +0100

    made changes to the folder strutures

--- admin/controllers/packages/create.php
+++ admin/controllers/packages/create.php
@@ -2,17 +2,9 @@
 
 session_start();
 
-$rootFolder = $rootFolder = $_SERVER['DOCUMENT_ROOT'] . "/kreativerock/";
+header('Content-Type: application/json');
 
-
-require_once $rootFolder . 'utils/errorhandler.php';
-require_once $rootFolder . 'utils/response.php';
-require_once $rootFolder . 'model/dbclass.php';
-require_once $rootFolder . 'model/model.php';
-require_once $rootFolder . 'model/dbFunctions.php';
-require_once $rootFolder . 'model/user.php';
-require_once $rootFolder . 'model/SmsPackage.php';
-require_once $rootFolder . 'utils/sanitize.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . "/kreativerock/utils/autoload.php";
 
 if ($_SESSION['elfuseremail'] === null || !isset($_SESSION['elfuseremail'])) {
     exit(badRequest(204,'Invalid session data. Proceed to login'));
@@ -22,7 +14,6 @@ if ($_SESSION['elfuseremail'] === null || !isset($_SESSION['elfuseremail'])) {
 $user = new User();
 $smsPackage = new SmsPackage();
 
-// $email = $_SESSION["elfuseremail"] ??  "abdulquadri.aq@gmail.com";
 $email = $_SESSION["elfuseremail"] ??  null;
 $res = $user->getUserInfo("email = '" . $email . "'");
 
--- admin/controllers/sms/checkoutsms.php
+++ admin/controllers/sms/checkoutsms.php
@@ -2,20 +2,9 @@
 
 session_start();
 
-$rootFolder = $rootFolder = $_SERVER['DOCUMENT_ROOT'] . "/kreativerock/";
+header('Content-Type: application/json');
 
-
-require_once $rootFolder . 'utils/errorhandler.php';
-require_once $rootFolder . 'utils/response.php';
-require_once $rootFolder . 'model/dbclass.php';
-require_once $rootFolder . 'model/model.php';
-require_once $rootFolder . 'model/dbFunctions.php';
-require_once $rootFolder . 'model/user.php';
-require_once $rootFolder . 'model/SmsPackage.php';
-require_once $rootFolder . 'model/SmsPurchase.php';
-require_once $rootFolder . 'model/SmsTransaction.php';
-require_once $rootFolder . 'model/Administration.php';
-require_once $rootFolder . 'utils/sanitize.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . "/kreativerock/utils/autoload.php";
 
 $user = new User();
 $smsPackage = new SmsPackage();
