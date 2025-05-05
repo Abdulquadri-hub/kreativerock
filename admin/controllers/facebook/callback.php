1846e44a330a4bc6cb35c1b1f42d0de8499078da
Abdul Quadri <abdulquadri.aq@gmail.com>
2025-05-05 09:15:24 +0100

    Login facebook ready

--- admin/controllers/facebook/callback.php
+++ admin/controllers/facebook/callback.php
@@ -5,9 +5,9 @@ header('Content-Type: application/json');
 
 require_once $_SERVER['DOCUMENT_ROOT'] . "/kreativerock/utils/autoload.php";
 
-if (isset($_SESSION['elfuseremail']) && $_SESSION['elfuseremail'] === null || !isset($_SESSION['elfuseremail'])) {
-    exit(badRequest(204,'Invalid session data. Proceed to login'));
-}
+// if (isset($_SESSION['elfuseremail']) && $_SESSION['elfuseremail'] === null || !isset($_SESSION['elfuseremail'])) {
+//     exit(badRequest(204,'Invalid session data. Proceed to login'));
+// }
    
 $facebook = new Facebook();
 
