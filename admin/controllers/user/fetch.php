4a841ec62e6c40d8d6e7bc9da06e6c0c1f04f051
Abdul Quadri <abdulquadri.aq@gmail.com>
2025-05-26 10:31:00 +0100

    created user

--- admin/controllers/user/fetch.php
+++ admin/controllers/user/fetch.php
@@ -3,9 +3,9 @@ session_start();
 header('Content-Type: application/json');
 require_once $_SERVER['DOCUMENT_ROOT'] . "/kreativerock/utils/autoload.php";
 
-if ($_SESSION['elfuseremail'] === null || !isset($_SESSION['elfuseremail'])) {
-    exit(badRequest(204,'Invalid session data. Proceed to login'));
-}
+// if ($_SESSION['elfuseremail'] === null || !isset($_SESSION['elfuseremail'])) {
+//     exit(badRequest(204,'Invalid session data. Proceed to login'));
+// }
 
 $user = new User();
 
